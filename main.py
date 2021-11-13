2d66abd1e3f5eb4510cbce7681333a4817a77a3d
KumarCodeAlt <94236521+KumarCodeAlt@users.noreply.github.com>
2021-11-13 22:10:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from custom_quick_sort import custom_quick_sort
 def main():
     input_data = input("Enter list of numbers separated by space: ")
     input_list = list(map(lambda x: int(x), input_data.split(" ")))
-    print("Entered data is :")
+    print("Entered data to sort is :")
     print(input_list)
     result = custom_quick_sort(input_list)
     print("Sorted result is: ")
