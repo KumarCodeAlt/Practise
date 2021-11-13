d93869fadb3b339697e82a89a8704e3f0e6cfb40
SHARAN KUMAR <45118936+SharanKumarCode@users.noreply.github.com>
2021-11-13 21:33:19 +0530

    added quick_sort functionality

---
+++ custom_quick_sort.py
@@ -0,0 +1,21 @@
+def custom_quick_sort(input_list):
+    length = len(input_list)
+    if len(input_list) < 2:
+        return input_list
+    else:
+        lhs_ = []
+        rhs_ = []
+        pivot = int(length/2)
+
+        for i in range(len(input_list)):
+            if i != pivot:
+                if input_list[i] <= input_list[pivot]:
+                    lhs_.append(input_list[i])
+                else:
+                    rhs_.append(input_list[i])
+
+        lhs = custom_quick_sort(lhs_)
+        rhs = custom_quick_sort(rhs_)
+        middle = input_list[pivot]
+
+        return [*lhs, middle, *rhs]
--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+from custom_quick_sort import custom_quick_sort
+
 
 def main():
     input_data = input("Enter list of numbers separated by space: ")
-    input_list = input_data.split(" ")
+    input_list = list(map(lambda x: int(x), input_data.split(" ")))
     print("Entered data is :")
     print(input_list)
+    result = custom_quick_sort(input_list)
+    print("Sorted result is: ")
+    print(result)
 
 
 if __name__ == "__main__":
