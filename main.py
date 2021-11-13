5e7c18226bc8507bfe03490471352624ba9ea8f0
SHARAN KUMAR <45118936+SharanKumarCode@users.noreply.github.com>
2021-11-13 20:34:04 +0530

    added main.py python file

---
+++ main.py
@@ -0,0 +1,10 @@
+
+def main():
+    input_data = input("Enter list of numbers separated by space: ")
+    input_list = input_data.split(" ")
+    print("Entered data is :")
+    print(input_list)
+
+
+if __name__ == "__main__":
+    main()
